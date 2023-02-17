3cf87fac7e4514ac545a3b7d3e335f397b5e40c8
cbarcan <87575852+cbarcan@users.noreply.github.com>
2023-02-17 09:18:25 +0200

    adding modals

--- scripts/app.js
+++ scripts/app.js
@@ -110,3 +110,31 @@ let swipeTimer = setInterval(swipeNext, 5000)
 window.addEventListener ("resize", () => {
   displaySwiperElements(value);
 })
+
+// Get the modal
+var modal = document.querySelectorAll(".modal");
+
+// Get the button that opens the modal
+var btn = document.querySelectorAll("#plus");
+
+// Get the <span> element that closes the modal
+var span = document.querySelectorAll(".close");
+
+console.log(modal, btn, span);
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+  modal[indexOf(btn)].style.display = "block";
+}
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
